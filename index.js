a2c623b4ccb85250d8f0759a64e47bd3a5a8c3a9
Meghan Prestemon <m.m.hares@gmail.com>
2017-09-13 15:36:47 -0700

    Install GraphQL and scaffold schema

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const root = require('./src/resolvers/RootResolver');
+const schema = require('./src/schema');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('aok');
-});
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+);
 
+app.use('/', (req, res) => {
+  res.sendStatus(200);
+});
 app.use((req, res) => {
   res.sendStatus(404);
 });
