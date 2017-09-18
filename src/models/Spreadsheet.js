0ab7bf23b9f0407513beded822c5025b68d7298f
Meghan Prestemon <m.m.hares@gmail.com>
2017-09-18 09:09:25 -0700

    Update Spreadsheet test

--- src/models/Spreadsheet.js
+++ src/models/Spreadsheet.js
@@ -11,7 +11,7 @@ class Spreadsheet {
   get sheets() {
     // const sheetNames = this.spreadSheetData.sheets.map(sheet => sheet.properties.title);
     const sheetDataArr = [data.SHEET1_DATA, data.SHEET2_DATA];
-    let sheetArr = sheetDataArr.map(sheetData => new Sheet(sheetData));
+    const sheetArr = sheetDataArr.map(sheetData => new Sheet(sheetData));
 
     return sheetArr;
   }
